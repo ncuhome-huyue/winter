2c228012733c63a95e7bdbaff0a331fc535c4e2e
ncuhome-huyue <32433710+ncuhome-huyue@users.noreply.github.com>
2018-02-27 20:40:51 +0800

    Create create.py

---
+++ sql/create.py
@@ -0,0 +1,33 @@
+from sqlite3 import *
+
+conn = connect('pinglun.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE COMPANY
+       (pl_id       TEXT    NOT NULL,
+       pl           TEXT     NOT NULL,
+       bookID       TEXT    NOT NULL,
+       fromID       TEXT    NOT NULL,
+       time         TEXT    NOT NULL,
+       zan          INT     NOT NULL,
+       cai          INT     NOT NULL);''')
+conn.commit()
+conn.close()
+
+conn = connect('zan_and_cai.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE COMPANY
+       (pl_id       TEXT    NOT NULL,
+       kind         TEXT    NOT NULL,
+       bookID       TEXT    NOT NULL,
+       fromID       TEXT    NOT NULL,
+       time         TEXT    NOT NULL);''')
+conn.commit()
+conn.close()
+
+conn = connect('collection.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE COMPANY
+       (bookID       TEXT    NOT NULL,
+       usernameID       TEXT    NOT NULL);''')
+conn.commit()
+conn.close()
